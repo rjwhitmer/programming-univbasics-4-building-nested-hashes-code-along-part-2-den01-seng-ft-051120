d39b73e1148cf80d466ac625db5f0ae1b0297c55
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-25 22:56:08 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -22,7 +22,9 @@ def update_status
 
   # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
   # Code your solution here:
-
+  
+  update_status[:montague][:hero][:status] = "dead"
+  update_status[:capulet][:hero][:status] = "dead"
 
 
 
